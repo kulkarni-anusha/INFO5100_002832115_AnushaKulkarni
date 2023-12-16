a898aa7f7909dea615ded4325c3e89501aa463f9
Anusha Kulkarni <141295185+kulkarni-anusha@users.noreply.github.com>
2023-12-16 09:51:06 -0800

    updated exercise8 and final project (#12)

--- FinalProject/src/main/java/com/example/finalprojectjava/ImageConversionException.java
+++ FinalProject/src/main/java/com/example/finalprojectjava/ImageConversionException.java
@@ -1,5 +1,7 @@
 package com.example.finalprojectjava;
 
+/*This is a custom exception class that extends the Java Exception class.
+ It is used to represent exceptions specific to image conversion operations in the application.*/
 // Exception class for ImageConversion
 public class ImageConversionException extends Exception {
     public ImageConversionException(String message, Throwable cause) {
